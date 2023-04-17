183727b75e33fae66fc80f414546ec77fc4ba747
DKReyes <dylankreyes@gmail.com>
2023-04-17 18:26:00 -0500

    Added title and about information

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,27 @@
 const Home = () => {
-    return <h1>Home</h1>;
+    return (
+      <>     
+        <h1>Hello, I'm Dylan Reyes</h1>
+        <h2>A fullstack web developer / software engineer</h2>
+        <button>Test</button>
+
+        <div>
+          <h2>About Me</h2>
+
+          <h4>Hello, I am Dylan Reyes.</h4>
+
+          <p>I have background in full stack, cloud computiing, and software development.</p>
+          
+          <p>I am familiar with React, Javascript, Python, C#, PHP, and Node.</p>
+
+          <p>I graduated from Red River College for computer science through their Bussiness Information Technology program. I had experience in coding through high school working with Java.</p>
+          
+          <p>Outside of coding I like to have many different hobbies. I love ice skating, kickboxing, and volleyball.
+            I like to go kickboxing five times a week and volleyball every sunday.
+          </p>
+        </div>
+      </>
+    )
   };
   
   export default Home;
