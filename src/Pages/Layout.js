ed6808f2e6c832450fe3ad0a44f800c4d8d3fb28
DKReyes <dylankreyes@gmail.com>
2023-04-17 22:51:48 -0500

    Fixed positions in navbar

--- src/Pages/Layout.js
+++ src/Pages/Layout.js
@@ -4,22 +4,24 @@ import '../Styles/Navbar.css';
 const Layout = () => {
   return (
     <>
-      <nav class='nav'>
-        <div class='nav-container'>
-          <p>Dylan Reyes</p>
-          <ul class='nav-ul'>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/blogs">Blogs</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+    <div class='nav-box'>
+      <nav class='nav-content'>
+          <div class='nav-container'>
+            <p>Dylan Reyes</p>
+            <ul class='nav-ul'>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/blogs">Blogs</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+    </div>
 
       <Outlet />
     </>
