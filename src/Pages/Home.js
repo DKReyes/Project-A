dd0970b649abfdce18bf0ed55b87e0a60bce3a51
DKReyes <dylankreyes@gmail.com>
2023-05-05 16:26:43 -0500

    Added information on technology

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -57,7 +57,11 @@ const Home = () => {
 
         <div>
           <h2>Technology</h2>
-
+          <p>
+            Throughout my career so far I've worked with all froms of front-end,  back-end,
+            and databasing with cloud computing in my most recent years. The current technologies I'm
+            profecient at are React, Javascript, C#, Python, AWS, and CSS.
+          </p>
         </div>
 
         <div>
