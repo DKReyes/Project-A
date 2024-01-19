026a6c819f30986ece464e66829c3ccadf138ebe
DKReyes <dylankreyes@gmail.com>
2024-01-18 23:17:58 -0600

    Added globe library

--- src/Pages/Technology.js
+++ src/Pages/Technology.js
@@ -1,3 +1,33 @@
+import {Cloud, renderSimpleIcon, renderImage, renderText, ICloud} from 'react-icon-cloud'
+// import jsIcon from "simple-icons/icons/javascript";
+
+// const iconTag = renderSimpleIcon({
+//   icon: jsIcon,
+// })
+
+// const imgTag = renderImg({
+//   imgProps: {
+//     src: 'https://openmoji.org/data/color/svg/1F44B.svg',
+//     alt: 'Globe',
+//     width: 100,
+//     height: 100
+//   },
+//   aProps: {
+//     href: 'https://openmoji.org',
+//     target: '_blank',
+//     rel: 'noreferrer',
+//   }
+// })
+
+// const textTag = renderText({
+//   text: 'hello'
+// })
+
+// const containerProps: ICloud['containerProps'] = {}
+// const canvasProps: ICloud['canvasProps'] = {}
+// const cloudProps: ICloud['options'] = {}
+
+
 const Technology = () => {
     return (
         <>
@@ -9,6 +39,15 @@ const Technology = () => {
                 profecient at are React, Javascript, C#, Python, AWS, and CSS.
                 </p>
             </div>
+            <Cloud 
+                // containerProps={containerProps} 
+                // canvasProps={canvasProps}
+                // options={options}
+            >
+                {/* {iconTag} */}
+                {/* {imgTag}
+                {textTag} */}
+            </Cloud>
         </>
     );
   };
