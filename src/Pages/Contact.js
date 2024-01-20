ae90defcaf076ff643bc1031ff3391fcf045fc99
DKReyes <dylankreyes@gmail.com>
2024-01-19 22:06:43 -0600

    Added background to contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,6 +1,16 @@
+import '../Styles/About.css'
+import '../Styles/Contact.css'
+
 const Contact = () => {
-    return <h1>Contact Me</h1>;
-  };
+    return (
+      <>
+        <div class='wrap'>
+          <div class='contact-main'>
+            <h1>Contact Me</h1>
+          </div>
+        </div>
+      </>
+    )};
   
   export default Contact;
   
